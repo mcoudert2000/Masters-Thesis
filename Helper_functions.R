2bfac3ad4ff51506da507c34579e29af8313f42e
mcoudert2000 <mcoudertjr@gmail.com>
2022-02-21 11:25:33 +0000

    added function to merge duplicate rows and replace with mean

---
+++ Helper_functions.R
@@ -0,0 +1,22 @@
+#HELPER FUNCTION
+
+
+#AVERAGE DUPLICATE ROWS
+#If two rows have the same name then take the mean of the two and replace it with that
+#Input should be have columns dat$GENENAME, samp1, samp2,..., sampn
+
+merge_duplicate_rows <- function(dat) {
+  library(data.table)
+  library(dplyr)
+  out <- aggregate(dat,by=list(dat$GENENAME),FUN=mean)
+  out <- out %>% mutate(GENENAME = Group.1) %>%
+    dplyr::select(-Group.1)
+  print(paste(dim(dat)[1] - dim(out)[1], ' rows removed.'))
+  rownames(out) <- out$GENENAME
+  return(dplyr::select(out, -GENENAME))
+}
+
+#test case
+test <- data.frame(GENENAME = c('A',"B","C","A","C"), s1 = 1:5, s2 = 2:6)
+test
+merge_duplicate_rows(test)
